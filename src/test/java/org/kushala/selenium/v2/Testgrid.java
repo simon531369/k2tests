bec929fca7fba8df039b3dc3b3b1c329cab3b9fd
simon531369 <simon531369@gmail.com>
2020-04-29 16:52:22 +0530

    testgrid

---
+++ src/test/java/org/kushala/selenium/v2/Testgrid.java
@@ -0,0 +1,28 @@
+package org.kushala.selenium.v2;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+
+public class Testgrid {
+	
+	@Test
+	public void test1() throws MalformedURLException, InterruptedException
+	{
+		DesiredCapabilities cap=DesiredCapabilities.chrome();
+		cap.setPlatform(Platform.LINUX);
+		URL url=new URL ("http://localhost:4446/wd/hub");
+		WebDriver driver=new RemoteWebDriver(url, cap);
+		driver.get("https://kushala.co.in");
+		Thread.sleep(5000);
+		driver.quit();
+		
+	}
+
+}
